457547921632d01da2ab6e2d947cc26a2a10dcf5
Sirintra Wannakeaw <sirintrawannakeaw@gmail.com>
2015-11-11 12:34:31 +0700

    Fix Defect 794 IN Pre To Post

--- app/app/controllers/migratePreToPostController.js
+++ app/app/controllers/migratePreToPostController.js
@@ -930,10 +930,13 @@ smartApp.controller('MigratePreToPostController', function(
                         //$ngBootbox.customDialog($scope.customDialogOptions);
                     }, 1000);
                 }
-                $scope.isVerify = false;
-                $scope.approveCode = "";
+                //$scope.isVerify = false;
+               // $scope.approveCode = "";
+                
             });
+             
         }
+        
     };
     $scope.focusPricePlanFilter = function() {
         if (!$scope.isLoadPricePlan) {
@@ -1209,6 +1212,7 @@ smartApp.controller('MigratePreToPostController', function(
     $scope.onCheckInputForVerify = function() {
         setTimeout(function() {
             $scope.isCheckInputForVerify = false;
+            $scope.isVerify = false;
             $scope.newOwner.birthDay = $('#birthDay').val();
             $scope.newOwner.expireDay = $('#expireDay').val();
 
