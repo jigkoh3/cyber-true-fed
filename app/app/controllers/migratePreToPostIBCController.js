85acfe5dc1e50a890a9194408d52d8136e4ee4e8
Waramun <booms_2p@hotmail.com>
2016-05-25 09:57:04 +0700

    Fixed DF1954 at Pre to Post IBC

--- app/app/controllers/migratePreToPostIBCController.js
+++ app/app/controllers/migratePreToPostIBCController.js
@@ -2529,7 +2529,7 @@ smartApp.controller('MigratePreToPostIBCController', function(
             console.log('ALL');
         }
         //$scope.titleOther2 = $scope.titleOther;
-        $scope.newOwner2.sex = $('#sex32BC').val();
+        $scope.newOwner2.sex = $('#sex32BC').val(); //20160525 Fixed DF1954
     };
 
     //ระบุผู้ใช้บัตร
