036cd53ef4b2c82fd4952d983bfec0634c1a5f99
Waramun <booms_2p@hotmail.com>
2016-09-09 23:15:23 +0700

    Fixed bug Error: [$rootScope:inprog] at Offer Page

--- app/app/controllers/addDeleteEditOfferController.js
+++ app/app/controllers/addDeleteEditOfferController.js
@@ -1745,7 +1745,7 @@ smartApp.controller('AddDeleteEditOfferController', function($scope,
         var setDatepicker = $scope.dataForEdit.param['expiration-date'].split("/");
         setTimeout(function() {
             $('#editCpExpirationDate').datepicker("setDate", new Date(Number(setDatepicker[2]) - 543,Number(setDatepicker[1]) - 1, Number(setDatepicker[0])) );
-        }, 500);  
+        }, 100);  
     };
 
     $scope.onEditCUGParam = function(item) {
