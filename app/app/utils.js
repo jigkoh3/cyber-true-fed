05eb6e644fd8fe2524136d5a3dbc32c7f2c264cc
xsam32 <xpvampire_sx@hotmail.com>
2016-09-09 14:41:33 +0700

    Test UMS pirnt v0.2

--- app/app/utils.js
+++ app/app/utils.js
@@ -155,11 +155,11 @@ function print_androidUMS(url) {
     // });
     // setTimeout(function(){
     // }, 3000);
-    urlx = "http://sff-uat.true.th:8980/webui/PDFs/AfterSaleReport.pdf";
+    // var urlx = "http://sff-uat.true.th:8980/webui/PDFs/AfterSaleReport.pdf";
     var urlLCS = localStorage.getItem('urlx');
     var webContextPath = getContextPath();
     //var urlx = window.location.origin+window.location.pathname+"pdfCanvasV4.html?action=none&url=" + urlLCS;
-    //var urlx = "http://sff-uat.true.th:8980/webui/report/view/pdf/" + urlLCS + ".action?clearData=N";
+    var urlx = "http://sff-uat.true.th:8980/webui/report/view/pdf/" + urlLCS + ".pdf";
     console.log(urlx);
     alert('Print URL:' + urlx);
     wms.printWeb(urlx, function(x) {
