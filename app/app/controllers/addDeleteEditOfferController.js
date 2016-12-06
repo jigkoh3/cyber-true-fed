5dc13e99fe2e3e1c60cc951a052e2a88173667bb
Waramun <booms_2p@hotmail.com>
2016-12-06 09:33:58 +0700

    Fixed DF2527 at Offer Page

--- app/app/controllers/addDeleteEditOfferController.js
+++ app/app/controllers/addDeleteEditOfferController.js
@@ -2843,6 +2843,8 @@ smartApp.controller('AddDeleteEditOfferController', function($scope,
                     }
                     break;
                 case "DISCOUNT":
+                case "FUB":
+                case "BARRING":
                     if ($scope.dataForEdit['param']['effective-date-type'] == "specify" && !$scope.dataForEdit['param']['effective-date-value']) {
                         $scope.disableSubmitAddOffer = true;
                     } else if ($scope.dataForEdit['param']['expiration-date-type'] == "specify" && !$scope.dataForEdit['param']['expiration-date-value']) {
@@ -2918,6 +2920,8 @@ smartApp.controller('AddDeleteEditOfferController', function($scope,
                     }
                     break;
                 case "DISCOUNT":
+                case "FUB":
+                case "BARRING":
                     if ($scope.offerEffectiveDate == "specify" && !$scope.addNewOfferEffectiveDate) {
                         $scope.disableSubmitAddOffer = true;
                     } else if ($scope.offerExpirationDate == "specify" && !$scope.addNewOfferExpirationDate) {
