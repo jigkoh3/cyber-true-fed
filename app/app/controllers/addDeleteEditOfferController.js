7a0be82d7249a9d6296bbf4837bbbbd9b48db8a9
Waramun <booms_2p@hotmail.com>
2017-01-09 12:17:49 +0700

    Change rule to show authorize check box at Offer Page

--- app/app/controllers/addDeleteEditOfferController.js
+++ app/app/controllers/addDeleteEditOfferController.js
@@ -3843,7 +3843,7 @@ smartApp.controller('AddDeleteEditOfferController', function($scope,
 
     $scope.checkAuthorize = function() {
         setTimeout(function() {
-            if (($scope.offerType == "U" && $scope.data.simData["account-category"] == "I") || ($scope.offerType == "D" && $scope.data.simData["account-category"] == "I")) {
+            if (($scope.offerType == "U" && $scope.data.simData["account-category"] == "I") || ($scope.offerType == "D" && $scope.data.simData["account-category"] == "I") || $scope.shopType == "0") {
                 //hide มอบอำนาจ
                 $('#divShowAuthorize').hide();
                 $('#authorize').prop('checked', false);
